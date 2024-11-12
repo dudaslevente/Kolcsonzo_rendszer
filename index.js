c399fb53b45156aa7e62fa9213fbe773c49d5ccf
KovagoLevente <kovagolevente@turr.hu>
2024-11-12 09:47:10 +0100

    igen

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var session = require('express-session');
 
 const coreRoutes = require('./modules/core');
 const userRoutes = require('./modules/users');
+const itemRoutes = require('./modules/items');
 
 const app = express();
 const port = process.env.PORT;
@@ -15,6 +16,7 @@ app.use(session({ secret: process.env.SESSION_SECRET}));
 
 app.use('/', coreRoutes);
 app.use('/users', userRoutes); 
+app.use('/items', itemRoutes);
 
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}...`);
--- modules/items.js
+++ modules/items.js
@@ -5,25 +5,36 @@ const uuid = require('uuid');
 const moment = require("moment");
 
 router.post('/reg', (req, res) => {
-
-
-
-    
-
     db.query(`INSERT INTO items (item_id, title, type, avalable) VALUES(?, ?, ?, ?)`, 
 	[uuid.v4(),title, type, available], 
         (err)=>{
         if (err){
             req.session.msg = 'Database error!';
             req.session.severity = 'danger';
-            res.redirect('/newdata');
+            res.redirect('/newtargyak');
             return
         }
-        req.session.msg = 'Steps registered!';
+        req.session.msg = 'Item registered!';
         req.session.severity = 'success';
-        res.redirect('/newdata');
+        res.redirect('/newtargyak');
         return
     })
+});
+router.get('/itemdatas', (req, res) => {
+
+    console.log(req.session.userID);
+    db.query(`SELECT * FROM stepdatas WHERE userID=?`, [req.session.userID,], (err, results)=>{
+        if (err) {
+            res.send(err);
+        }
+
+        else{
+            results.forEach(item => {
+                item.date = moment(results.date).format("YYYY-MM-DD")
+            });
+            res.send(results);
+        }
+    });
  
 
 });
