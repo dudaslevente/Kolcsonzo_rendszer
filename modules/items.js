58e0f132b7fe2602ef604b14e027bfac6419c34c
dudaslevente <dudaslevente@turr.hu>
2024-11-12 19:09:35 +0100

    Tárgyak feltöltése

--- modules/items.js
+++ modules/items.js
@@ -7,35 +7,26 @@ const moment = require("moment");
 router.post('/newtargyak', (req, res) => {
     let { title, type, available } = req.body;
 
-     if (!title || !type || !available) {
+    if (!title || type=== undefined || available === undefined) {
         req.session.msg = 'Missing data!';
         req.session.severity = 'danger';
-        res.redirect('/newtargyak');
-        return
+        return res.redirect('/newtargyak');
     }
 
-    db.query(`SELECT * FROM items WHERE item_id=?`, [req.session.item_id, title, type, available], (err, results) => {
-        if (err){
-            req.session.msg = 'Database error!';
-            req.session.severity = 'danger';
-            res.redirect('/newtargyak');
-            return
-        }
-
-        db.query(`INSERT INTO items (item_id, title, type, available) VALUES(?,?,?,?)`, [req.session.item_id, title, type, available], (err, results)=>{
-            if (err){
+    db.query(`INSERT INTO items (title, type, available) VALUES (?, ?, ?)`,
+        [title, type ? 'könyv' : 'film', available ? 1 : 0],
+        (err) => {
+            if (err) {
                 req.session.msg = 'Database error!';
                 req.session.severity = 'danger';
-                res.redirect('/newtargyak');
-                return
+                return res.redirect('/newtargyak');
             }
+
             req.session.msg = 'Item inserted!';
             req.session.severity = 'success';
             res.redirect('/newtargyak');
-            return
-        });
-        return
-    });
+        }
+    );
 });
 
 module.exports = router;
